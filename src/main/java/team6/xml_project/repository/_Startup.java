ee46851f2832fb826da3ca070adfe0c97e418e46
Nemanja Jankovic <nemanja997@gmail.com>
2020-02-06 19:17:06 +0100

    Added initial data generation to application startup

--- src/main/java/team6/xml_project/repository/_Startup.java
+++ src/main/java/team6/xml_project/repository/_Startup.java
@@ -13,22 +13,62 @@ import java.util.Arrays;
 import java.util.Collections;
 
 @Component
-public class _Startup {//implements ApplicationRunner {
+public class _Startup implements ApplicationRunner {
 
     @Autowired
     private UserRepository userRepository;
 
     @Transactional
-    //@Override
-    public void run(ApplicationArguments args) throws Exception {
-        User u1 = new User("u1@mail.com", "$2a$10$PH7tn1C1P8x2N6hEbPvXwOgD88Bz5Nanti7mklRQTg6BdD.qoJnAy", true, "Name1", "Surname1", Arrays.asList("math", "physics"), Role.ROLE_AUTHOR);
-        User u2 = new User("u2@mail.com", "$2a$10$PH7tn1C1P8x2N6hEbPvXwOgD88Bz5Nanti7mklRQTg6BdD.qoJnAy", true, "Name2", "Surname2", Arrays.asList("math", "biology"), Role.ROLE_AUTHOR);
-        User u3 = new User("u3@mail.com", "$2a$10$PH7tn1C1P8x2N6hEbPvXwOgD88Bz5Nanti7mklRQTg6BdD.qoJnAy", true, "Name3", "Surname3", Arrays.asList("chemistry", "biology"), Role.ROLE_AUTHOR);
-        User e1 = new User("editor@mail.com", "$2a$10$PH7tn1C1P8x2N6hEbPvXwOgD88Bz5Nanti7mklRQTg6BdD.qoJnAy", true, "Name4", "Surname4", Arrays.asList("language", "biology"), Role.ROLE_EDITOR);
-        userRepository.save(u1);
-        userRepository.save(u2);
-        userRepository.save(u3);
-        userRepository.save(e1);
+    @Override
+    public void run(ApplicationArguments args) {
+        if (!userRepository.findUserByEmail("tim6.xml+u1@gmail.com").isPresent()) {
+            User user = new User("tim6.xml+u1@gmail.com",
+                    "$2a$10$PH7tn1C1P8x2N6hEbPvXwOgD88Bz5Nanti7mklRQTg6BdD.qoJnAy", // "password"
+                    true, "John", "Smith",
+                    Arrays.asList("math", "physics", "algebra", "set theory", "mathematics", "quantum theory"), Role.ROLE_AUTHOR);
+            userRepository.save(user);
+        }
+
+        if (!userRepository.findUserByEmail("tim6.xml+u2@gmail.com").isPresent()) {
+            User user = new User("tim6.xml+u2@gmail.com",
+                    "$2a$10$PH7tn1C1P8x2N6hEbPvXwOgD88Bz5Nanti7mklRQTg6BdD.qoJnAy", // "password"
+                    true, "Jane", "Smith",
+                    Arrays.asList("lasers", "physics", "theoretical physics", "astronomy", "quantum theory"), Role.ROLE_AUTHOR);
+            userRepository.save(user);
+        }
+
+        if (!userRepository.findUserByEmail("tim6.xml+u3@gmail.com").isPresent()) {
+            User user = new User("tim6.xml+u3@gmail.com",
+                    "$2a$10$PH7tn1C1P8x2N6hEbPvXwOgD88Bz5Nanti7mklRQTg6BdD.qoJnAy", // "password"
+                    true, "Robert", "Rich",
+                    Arrays.asList("biology", "zoology", "chemistry", "organic chemistry", "bacteria", "viruses"), Role.ROLE_AUTHOR);
+            userRepository.save(user);
+        }
+
+        if (!userRepository.findUserByEmail("tim6.xml+u4@gmail.com").isPresent()) {
+            User user = new User("tim6.xml+u4@gmail.com",
+                    "$2a$10$PH7tn1C1P8x2N6hEbPvXwOgD88Bz5Nanti7mklRQTg6BdD.qoJnAy", // "password"
+                    true, "Jim", "Cook",
+                    Arrays.asList("language", "medicine", "chemistry", "organic chemistry", "history"), Role.ROLE_AUTHOR);
+            userRepository.save(user);
+        }
+
+
+        if (!userRepository.findUserByEmail("tim6.xml+e1@gmail.com").isPresent()) {
+            User user = new User("tim6.xml+e1@gmail.com",
+                    "$2a$10$PH7tn1C1P8x2N6hEbPvXwOgD88Bz5Nanti7mklRQTg6BdD.qoJnAy", // "password"
+                    true, "Tim", "Stone",
+                    Arrays.asList("radiology", "medicine", "heart surgery", "bacteria", "viruses"), Role.ROLE_EDITOR);
+            userRepository.save(user);
+        }
+
+        if (!userRepository.findUserByEmail("tim6.xml+e2@gmail.com").isPresent()) {
+            User user = new User("tim6.xml+e2@gmail.com",
+                    "$2a$10$PH7tn1C1P8x2N6hEbPvXwOgD88Bz5Nanti7mklRQTg6BdD.qoJnAy", // "password"
+                    true, "Billy", "Mays",
+                    Arrays.asList("economy", "taxes", "capitalism", "sales"), Role.ROLE_EDITOR);
+            userRepository.save(user);
+        }
 
         userRepository.flush();
     }
